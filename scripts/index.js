69226efcfa22d06a1a4fc4bec2cacb48d7915bb8
delmas cox <delmaslcox1@gamil.com>
2025-08-06 19:58:23 -0400

    like button

--- scripts/index.js
+++ scripts/index.js
@@ -59,6 +59,11 @@ function getCardElement(data) {
   cardImageEl.alt = data.name;
   cardTitleEl.textContent = data.name;
 
+  const cardLikeBtnEl = cardElement.querySelector(".card__like-button");
+  cardLikeBtnEl.addEventListener("click", () => {
+    cardLikeBtnEl.classList.toggle("card__like-button_active");
+  });
+
   return cardElement;
 }
 
